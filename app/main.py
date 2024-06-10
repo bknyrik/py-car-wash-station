7e547b92a25fc33c8e713cbe727fcf63ac770dfe
Bogdan Knyrik <knyrikkolesnichenko2004@gmail.com>
2024-06-10 22:26:36 +0300

    Solution 4.0

--- app/main.py
+++ app/main.py
@@ -1,21 +1,23 @@
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str
-                 ) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: int,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int
-                 ) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: int,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
+    ) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
@@ -31,8 +33,10 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center, 1)
+        return round(
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * self.average_rating / self.distance_from_city_center,
+            1)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
@@ -40,5 +44,7 @@ class CarWashStation:
     def rate_service(self, new_mark: int) -> None:
         sum_marks = self.average_rating * self.count_of_ratings
         self.average_rating = round(
-            (sum_marks + new_mark) / (self.count_of_ratings + 1), 1)
+            (sum_marks + new_mark) / (self.count_of_ratings + 1),
+            1
+        )
         self.count_of_ratings += 1
